a34b88c634ff36627e3fb309d3a523c2abd20809
User <User@DESKTOP-R6A59AH>
2024-02-18 22:56:58 +0500

    Add project files.

---
+++ Files_Nm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text.Json.Serialization;
+
+namespace Files_Nm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            DirectoryInfo directory = new DirectoryInfo("D:/Darslar C#/Muhammadkarim ustozimning darslari/");
+            FileInfo[] fileInfo = directory.GetFiles();
+            for(int i = 0; i < fileInfo.Length; i++)
+            {
+                Console.WriteLine($"{i} - {fileInfo[i].Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
